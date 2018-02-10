1915614092d6d043131935bbe1ae76a17e1e3787
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2018-02-10 14:50:11 +0200

    MNIST coding challenge
    add loadMNIST method to load mnist from idx binary files
    
    add main.py to be the entry point

---
+++ mnist/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('../NeuralNetwork')
+import matrix
+import nn
+
+
+"""
+
+neural network stuff will be here
+
+wait for it
+
+"""
---
+++ mnist/mnist.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import numpy as np
+import struct
+
+def loadMNIST(dataset, callback):
+    # Mnist Files
+    files = {
+        'train_images': 'train-images-idx3-ubyte',
+        'train_labels': 'train-labels-idx1-ubyte',
+        'test_images': 't10k-images-idx3-ubyte',
+        'test_labels': 't10k-labels-idx1-ubyte',
+    }
+
+    # Datasets to load
+    if dataset == 'training':
+        fname_img = files.get('train_images')
+        fname_lbl = files.get('train_labels')
+    elif dataset == 'testing':
+        fname_img = files.get('test_images')
+        fname_lbl = files.get('test_labels')
+    else:
+        raise ValueError("Dataset must be 'training' or 'testing'")
+
+    with open(fname_lbl, 'rb') as flbl:
+        magic, num = struct.unpack('>II', flbl.read(8))
+        lbl = np.fromfile(flbl, dtype=np.int8)
+
+    with open(fname_img, 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack('>IIII', fimg.read(16))
+        img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+
+    get_img = lambda idx: (lbl[idx], img[idx])
+
+    for i in range(len(lbl)):
+        yield get_img(i)
+
+    callback()
+
+def show(image):
+    #Render a given numpy.uint8 2D array of pixel data.
+    plt.imshow(image, interpolation='nearest', cmap=plt.cm.gray_r)
+    plt.show()
+
+# training_data = list(loadMNIST('training', lambda: print("Done")))
+# print(len(training_data))
+# label, pixles = training_data[0]
+# print(label)
+# print(pixles.shape)
+# show(pixles)
